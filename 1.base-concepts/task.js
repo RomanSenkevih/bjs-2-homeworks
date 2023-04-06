b217e82f4287366d98359ae885a3e7e5a44c7b7c
roman <gosahorosij328@gmail.com>
2023-04-06 19:35:08 +0300

    домашнее задание 1 задача 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,23 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b**2 - 4*a*c;
+  let x1 = 0;
+  let x2 = 0;
+        
+        if (d === 0){
+          x1 = (-b + Math.sqrt(d)) / 2*a;
+          arr[0] = x1;
+      }
+        if (d > 0){
+          x1 = (-b + Math.sqrt(d)) / 2*a;
+          x2 = (-b - Math.sqrt(d)) / 2*a;
+           arr[0] = x1;
+           arr[1] = x2;
+        }
+        if (d < 0){
+          arr = [];
+        }
   return arr;
 }
 
