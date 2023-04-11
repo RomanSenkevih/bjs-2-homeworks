bb26472cc2d1308bc1dc3f57de335a4d9d71f661
roman <gosahorosij328@gmail.com>
2023-04-11 18:57:45 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    return arr1.every((element, index, array) => element === arr2[index] && array.length === arr2.length); 
+  }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+  function getUsersNamesInAgeRange(users, gender) {
+    let arrayGender = users.filter((element) => element.gender === gender);
+    let sumAge = (arrayGender.reduce((acc, element) => acc + element.age, 0)) / arrayGender.length;
+    let result = isNaN(sumAge) === true ? 0 : sumAge;
+    return result;
 }
